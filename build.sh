68c24766053759c43a3097eb230db74b4f82a470
daniel-platform <daniel@platform.sh>
2019-03-30 17:41:12 -0400

    Set default version to 6.0.2

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@
 
 # Desired version can be set by means of an enviromental variable
 if [ -z "$GRAFANA_VERSION" ]; then 
-	# Default to Grafana 5.2.1
-	GRAFANA_VERSION=5.2.1; 
+	# Default to Grafana 6.0.2
+	GRAFANA_VERSION=6.0.2; 
 fi
 
 GRAFANA_DOWNLOAD_URI="https://s3-us-west-2.amazonaws.com/grafana-releases/release"
